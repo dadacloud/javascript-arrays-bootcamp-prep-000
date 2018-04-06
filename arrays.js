47a74b569537fe77cd147d3d75a85b6d67e3f1f6
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-06 19:18:57 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
+
+function addElementToBeginningOfArray(chocolateBars, string){
+ var candy = [string,...chocolateBars];
+ return candy;
+
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, string){
+  return chocolateBars.unshift(string);
+}
+
+function addElementToEndOfArray(chocolateBars, string){
+ var morechocolateBars = [...chocolateBars, string];
+ return morechoclateBars;
+}
+
+function destructivelyAddElementToEndOfArray (chocolateBars, string){
+  return chocolateBars.push(string);
+}
+
+function accessElementInArray(chocolateBars,index){
+  return chocolateBars [index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.shift();
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  var slicedbars = chocolateBars.slice(1);
+  return slicedbars;
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  return chocolateBars.pop();
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0,chocolateBars.length-1);
+}
+
+
